22434fb21df8ac77bfb70a39871b97b02c9447f9
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-07-19 22:58:09 +0000

    Minor tweak email subject line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ default from: 'no-reply@jungle.com'
 
   def order_email(order)
     @order = order
-    mail(to: @order.email, subject: @order.id)
+    mail(to: @order.email, subject: "Order No. #{@order.id} Confirmation")
   end
 end
