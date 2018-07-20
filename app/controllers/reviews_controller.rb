b2c7423f9023da682a0818b007d01ea494444af1
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-07-20 20:18:15 +0000

    Implement delete review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,15 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create(review_params)
     @review.user = current_user
-    if @review.save
-      redirect_to '/'
-    else
-      redirect_to products_path(@product)
-    end
+    @review.save
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
   end
 
   private
