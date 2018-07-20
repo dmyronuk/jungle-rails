98478b8e8902d9b54ffd20f388412fbb4ea075ef
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-07-20 19:14:37 +0000

    Add review form disabled for users not logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,12 @@
 class ReviewsController < ApplicationController
+  before_action :authorize
+
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create(review_params)
     @review.user = current_user
     if @review.save
-      redirect_to "/"
+      redirect_to '/'
     else
       redirect_to products_path(@product)
     end
@@ -14,5 +16,11 @@ class ReviewsController < ApplicationController
 
     def review_params
       params.require(:review).permit(:rating, :description)
+    end
+
+    def authorize
+      if !current_user
+        redirect_to '/'
+      end
     end
 end
