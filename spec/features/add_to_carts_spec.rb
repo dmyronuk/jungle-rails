ec133d186c51c53e848cee3d4c02d348eaa63d78
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-07-24 16:44:09 +0000

    Implement test that users can click add item button and item is added to cart

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor clicks on the cart link and one item is added to cart", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,13 +17,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   end
 
 
-  scenario "They see all products" do
+  scenario "Visitor clicks on the cart link" do
     # ACT
     visit root_path
 
     # DEBUG / VERIFY
-    #save_screenshot
-    expect(page).to have_css 'article.product', count: 10
+    save_screenshot
+    find('.product', match: :first).find('.btn-primary').click
+    cart = find(:xpath, "//a[@href='/cart']")
+    expect(cart).to have_text 'My Cart (1)'
   end
 
 end
--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,14 +16,12 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-
-  scenario "They see all products" do
+  scenario "Visitor clicks on a product" do
     # ACT
     visit root_path
 
-    # DEBUG / VERIFY
+    find('.product', match: :first).find('header').click
     #save_screenshot
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css '.product-detail', count: 1
   end
-
 end
